890ed53f0fafee89a3fe248ff8df1b451e97745c
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-03-07 21:26:14 -0400

    Added name field in some empty types

--- src/index.js
+++ src/index.js
@@ -15719,6 +15719,8 @@ export const InteractionCounter = new GraphQLObjectType({
   description: 'A summary of how users have interacted with this CreativeWork. In most cases, authors will use a subtype to specify the specific type of interaction.',
   name: 'InteractionCounter',
   fields: () => ({
+    name,
+    /* TODO: Complete field list */
   }),
 })
 export const InternetCafe = new GraphQLObjectType({
@@ -18586,6 +18588,8 @@ export const MonetaryAmount = new GraphQLObjectType({
   description: 'A monetary value or range. This type can be used to describe an amount of money such as $50 USD, or a range as in describing a bank account being suitable for a balance between £1,000 and £1,000,000 GBP, or the value of a salary, etc. It is recommended to use PriceSpecification Types to describe the price of an Offer, Invoice, etc.',
   name: 'MonetaryAmount',
   fields: () => ({
+    name,
+    /* TODO: Complete field list */
   }),
 })
 export const Mosque = new GraphQLObjectType({
